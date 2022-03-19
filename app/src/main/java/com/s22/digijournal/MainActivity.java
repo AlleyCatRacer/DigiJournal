7a4d1156a69bfcf399b24bd01fb80534b120ea27
Aldís Eir Hansen <74299218+AlleyCatRacer@users.noreply.github.com>
2022-03-19 09:10:05 +0100

    Setup
    Blank project creation

---
+++ app/src/main/java/com/s22/digijournal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.s22.digijournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
