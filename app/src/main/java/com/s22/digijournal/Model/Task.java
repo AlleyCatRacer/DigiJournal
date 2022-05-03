7cf427ae70e9d8ad7bd39156317bd3287cc45870
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-03 10:18:08 +0200

    Formatted string methods for views

--- app/src/main/java/com/s22/digijournal/Model/Task.java
+++ app/src/main/java/com/s22/digijournal/Model/Task.java
@@ -4,6 +4,10 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @Entity
 public class Task
 {
@@ -44,15 +48,47 @@ public class Task
     {
         return dateAdded;
     }
+    
+    public String getDateAddedFormatted()
+    {
+        Date date = new Date(Long.parseLong(String.valueOf(dateAdded)));
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String formatted = format.format(date);
+        return formatted;
+    }
 
     public long getDateEdited()
     {
         return dateEdited;
     }
+    
+    public String getDateEditedFormatted()
+    {
+        if (dateEdited != 0)
+        {
+            Date date = new Date(Long.parseLong(String.valueOf(dateEdited)));
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            String formatted = format.format(date);
+            return formatted;
+        }
+        return "N/A";
+    }
 
     public long getDeadline()
     {
         return deadline;
+    }
+    
+    public String getDeadlineFormatted()
+    {
+        if (deadline != 0)
+        {
+            Date date = new Date(Long.parseLong(String.valueOf(deadline)));
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+            String formatted = format.format(date);
+            return formatted;
+        }
+        return "N/A";
     }
 
     public void setIsDone(boolean done)
@@ -63,6 +99,11 @@ public class Task
     public void setDescription(String description)
     {
         this.description = description;
+    }
+    
+    public void setDateAdded(long dateAdded)
+    {
+        this.dateAdded = dateAdded;
     }
 
     public void setDateEdited(long dateEdited)
