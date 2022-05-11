a9ee73d40f602183c7ae4da558a1565f10918657
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-11 10:23:09 +0200

    Nav support XMLs

--- app/src/main/java/com/s22/digijournal/NavigationDrawer.java
+++ app/src/main/java/com/s22/digijournal/NavigationDrawer.java
@@ -45,7 +45,7 @@ public class NavigationDrawer extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
