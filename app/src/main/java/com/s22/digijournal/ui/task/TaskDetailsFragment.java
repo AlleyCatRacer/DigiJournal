8a38133893a256814b8b18499e530d84a83b06bb
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-11 16:40:35 +0200

    Edit task java + nav update

--- app/src/main/java/com/s22/digijournal/ui/task/TaskDetailsFragment.java
+++ app/src/main/java/com/s22/digijournal/ui/task/TaskDetailsFragment.java
@@ -1,66 +1,55 @@
 package com.s22.digijournal.ui.task;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.s22.digijournal.R;
+import com.s22.digijournal.databinding.FragmentTaskDetailsBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TaskDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TaskDetailsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TaskDetailsFragment() {
-        // Required empty public constructor
+public class TaskDetailsFragment extends Fragment
+{
+	private FragmentTaskDetailsBinding binding;
+    private TaskViewModel viewModel;
+    private TextView taskName;
+    private TextView description;
+    private TextView deadline;
+    
+    public TaskDetailsFragment()
+    {
+    
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TaskDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TaskDetailsFragment newInstance(String param1, String param2) {
-        TaskDetailsFragment fragment = new TaskDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        viewModel = new ViewModelProvider(requireActivity()).get(TaskViewModel.class);
+        binding = FragmentTaskDetailsBinding.inflate(inflater, container, false);
+        
+        taskName = binding.taskDetailsTaskName;
+        description = binding.taskDetailsDetailText;
+        deadline = binding.taskDetailsDeadline;
+        
+        return binding.getRoot();
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        binding.fab.setOnClickListener(v -> NavHostFragment.findNavController(TaskDetailsFragment.this).navigate(R.id.action_nav_task_details_to_nav_add_task));
+        binding.taskDetailsEditButton.setOnClickListener(v -> NavHostFragment.findNavController(TaskDetailsFragment.this).navigate(R.id.action_nav_task_details_to_nav_task_edit));
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_details, container, false);
+    
+    @Override public void onDestroyView()
+    {
+        super.onDestroyView();
+        binding = null;
     }
 }
