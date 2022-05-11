4f9a631ccb75adb683d560fbc17144d99a814404
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-11 10:41:13 +0200

    Nav menu items

--- app/src/main/java/com/s22/digijournal/NavigationDrawer.java
+++ app/src/main/java/com/s22/digijournal/NavigationDrawer.java
@@ -33,7 +33,7 @@ public class NavigationDrawer extends AppCompatActivity
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_categories, R.id.nav_lists, R.id.nav_tasks)
+                R.id.nav_home, R.id.nav_tasks)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
