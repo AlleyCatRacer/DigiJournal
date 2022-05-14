81af7463911e2a2e407cf4a1be63bac1bbc92d2b
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-14 10:53:18 +0200

    Renaming unit test

--- app/src/test/java/com/s22/digijournal/ModelTaskUnitTest.java
+++ app/src/test/java/com/s22/digijournal/ModelTaskUnitTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExampleUnitTest
+public class ModelTaskUnitTest
 {
 	private ModelTask t1;
 	private ModelTask t2;
