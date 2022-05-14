10eb9b3ac0ab5173618a64b3b82605989fe4c01f
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2022-05-14 10:37:12 +0200

    Task model class unit test

--- app/src/main/java/com/s22/digijournal/ModelTask.java
+++ app/src/main/java/com/s22/digijournal/ModelTask.java
@@ -5,111 +5,125 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.sql.Date;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Entity(tableName = "task_table")
 public class ModelTask
 {
-    @PrimaryKey(autoGenerate = true)
-    private int ID;
-    private String name;
-    private String description;
-    private long deadline;
-    @ColumnInfo(name = "status")
-    private boolean completed;
-    
-    public ModelTask()
-    {
-    
-    }
-    
-    public ModelTask(String name, String description, String deadline)
-    {
-        this.name = name;
-        this.description = description;
-        this.deadline = formatDeadline(deadline);
-        completed = false;
-    }
-
-    public int getID()
-    {
-        return ID;
-    }
-    
-    public void setID(int ID)
-    {
-        this.ID = ID;
-    }
-    
-    public String getName()
-    {
-        return name;
-    }
-    
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-    
-    public boolean isCompleted()
-    {
-        return completed;
-    }
-
-    public void setCompleted(boolean isDone)
-    {
-        this.completed = isDone;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
-
-    public long getDeadline()
-    {
-        return deadline;
-    }
-
-    public void setDeadline(long deadline)
-    {
-        Date date = new Date(deadline);
-        Date now = new Date(Calendar.getInstance().getTime().getTime());
-        
-        if (date.after(now))
-        {
-            this.deadline = deadline;
-        }
-    }
-    
-    public String getDeadlineFormatted()
-    {
-        if (deadline != 0)
-        {
-            java.sql.Date date = new java.sql.Date(Long.parseLong(String.valueOf(deadline)));
-            DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-            return format.format(date);
-        }
-        return "N/A";
-    }
-    
-    public long formatDeadline(String deadline)
-    {
-        if (deadline.isEmpty() || deadline.equals("N/A"))
-        {
-            return 0;
-        }
-    
-        String trim = deadline.replaceAll("/","");
-        java.sql.Date date = new Date(Long.parseLong(trim));
-        
-        return date.getTime();
-    }
+	@PrimaryKey(autoGenerate = true)
+	private int ID;
+	private String name;
+	private String description;
+	private long deadline;
+	@ColumnInfo(name = "status")
+	private boolean completed;
+	
+	public ModelTask()
+	{
+	
+	}
+	
+	public ModelTask(String name, String description, String deadline)
+	{
+		this.name = name;
+		this.description = description;
+		this.deadline = formatDeadline(deadline);
+		completed = false;
+	}
+	
+	public int getID()
+	{
+		return ID;
+	}
+	
+	public void setID(int ID)
+	{
+		this.ID = ID;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public boolean isCompleted()
+	{
+		return completed;
+	}
+	
+	public void setCompleted(boolean isDone)
+	{
+		this.completed = isDone;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+	
+	public long getDeadline()
+	{
+		return deadline;
+	}
+	
+	public void setDeadline(long deadline)
+	{
+		if (isAfter(deadline))
+		{
+			this.deadline = deadline;
+		}
+	}
+	
+	public String getDeadlineFormatted()
+	{
+		if (getDeadline() <= 0)
+		{
+			return "N/A";
+		}
+		
+		return new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date(deadline * 1000));
+	}
+	
+	public long formatDeadline(String stringDeadline)
+	{
+		if (stringDeadline.isEmpty() || stringDeadline.equals("N/A"))
+		{
+			return 0;
+		}
+		String withSecs = stringDeadline + " 23:59:59";
+		LocalDate date = LocalDate.parse(withSecs, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+		Instant temp = date.atTime(LocalTime.MAX).atZone(ZoneId.systemDefault()).toInstant();
+		
+		return new java.util.Date(temp.getEpochSecond()).getTime();
+	}
+	
+	public boolean isAfter(long epoch)
+	{
+		Date date = new Date(epoch * 1000);
+		Instant now = Instant.now();
+		Date tempNow = new Date(now.toEpochMilli());
+		return date.after(tempNow);
+	}
 }
+/*
+        ZonedDateTime ldate = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+                                   .atZone(ZoneId.of("Europe/Paris"));
+long time = ldate.toInstant().toEpochMilli();
+System.out.println(time);
+*/
--- app/src/test/java/com/s22/digijournal/ExampleUnitTest.java
+++ app/src/test/java/com/s22/digijournal/ExampleUnitTest.java
@@ -1,17 +1,104 @@
 package com.s22.digijournal;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+public class ExampleUnitTest
+{
+	private ModelTask t1;
+	private ModelTask t2;
+	private ModelTask t3;
+	private List<ModelTask> tasks;
+	
+	@Before public void setUp()
+	{
+		t1 = new ModelTask("Task 1", "Short description ay", "01/01/2023");
+		t2 = new ModelTask("Task 2", "", "N/A");
+		t3 = new ModelTask("Task 3", "V.short desc.", "");
+		t1.setID(1);
+		t2.setID(2);
+		t3.setID(2);
+		tasks = new ArrayList<>();
+		tasks.add(t1);
+		tasks.add(t2);
+		tasks.add(t3);
+	}
+	
+	@After public void tearDown()
+	{
+		t1 = null;
+		t2 = null;
+		t3 = null;
+	}
+	
+	@Test public void taskListIsNotEmptyWhenNotEmpty()
+	{
+		assertFalse(tasks.isEmpty());
+	}
+	
+	@Test public void gettingSetTaskID()
+	{
+		assertEquals(1, t1.getID());
+	}
+	
+	@Test public void changingTaskNameToTaskNr1()
+	{
+		t1.setName("Task nr. 1");
+		assertEquals("Task nr. 1", t1.getName());
+	}
+	
+	@Test public void gettingEmptyDescription()
+	{
+		assertEquals("", t2.getDescription());
+	}
+	
+	@Test public void gettingNotEmptyDescription()
+	{
+		assertEquals("Short description ay", t1.getDescription());
+	}
+	
+	@Test public void gettingDeadlineLongFromString()
+	{
+		assertEquals(1672613999L, t1.getDeadline());
+	}
+	
+	@Test public void gettingDeadlineFormattedFromStringWithNoDeadline()
+	{
+		assertEquals("N/A", t3.getDeadlineFormatted());
+	}
+	
+	@Test public void settingDeadlineLong1672534800()
+	{
+		t3.setDeadline(1672534800L);
+		assertEquals(1672534800, t3.getDeadline());
+	}
+	
+	@Test public void gettingFormattedDeadlineFromExistingDeadline()
+	{
+		assertEquals("01/01/2023", t1.getDeadlineFormatted());
+	}
+	
+	@Test public void gettingDeadlineLong()
+	{
+		assertEquals(1672613999, t1.getDeadline());
+	}
+	
+	@Test public void cannotSetDeadlineBeforeNow()
+	{
+		t3.setDeadline(t3.formatDeadline("13/05/2022"));
+		assertEquals(0, t3.getDeadline());
+	}
+	
+	@Test public void canSetDeadlineAfter()
+	{
+		t3.setDeadline(t3.formatDeadline("01/01/3000"));
+		assertEquals(32503762799L, t3.getDeadline());
+	}
 }
